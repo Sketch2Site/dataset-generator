8b34bb913c19c1f727811e6b8f1ab1d0e8306d02
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-02-26 11:27:32 +0530

    feat(navbar): nested configs

--- assets/lib/navbar.js
+++ assets/lib/navbar.js
@@ -1,18 +1,77 @@
-function generateNavbar(offset) {
+function generateNavbar(entropy) {
+  const offset = {
+    outerBox: {
+      x: 10,
+      y: 10,
+    },
+
+    leftLogo: {
+      x: 10,
+      y: 10,
+    },
+  };
+
+  const dimension = {
+    outerBox: {
+      width: 1000,
+      height: 70,
+    },
+
+    leftLogo: {
+      width: 150,
+      height: 50,
+    },
+  };
+
+  // outer box for navbar component
   svg.appendChild(
     rc.polygon(
       [
-        [10, 10],
-        [200, 10],
-        [200, 100],
-        [10, 100],
+        [offset.outerBox.x, offset.outerBox.y],
+        [offset.outerBox.x + dimension.outerBox.width, offset.outerBox.y],
+
+        [
+          offset.outerBox.x + dimension.outerBox.width,
+          offset.outerBox.y + dimension.outerBox.height,
+        ],
+        [offset.outerBox.x, offset.outerBox.y + dimension.outerBox.height],
       ],
       {
         stroke: "gray",
-        hachureAngle: 65,
-        fill: "rgba(0,0,0,0.6)",
+        fill: "rgba(255,255,255,0.6)",
+        fillStyle: "solid",
         roughness: 1,
-        strokeWidth: 1,
+        strokeWidth: 2,
+      }
+    )
+  );
+
+  svg.appendChild(
+    rc.polygon(
+      [
+        [
+          offset.outerBox.x + offset.leftLogo.x,
+          offset.outerBox.y + offset.leftLogo.y,
+        ],
+        [
+          offset.outerBox.x + offset.leftLogo.x + dimension.leftLogo.width,
+          offset.outerBox.y + offset.leftLogo.y,
+        ],
+        [
+          offset.outerBox.x + offset.leftLogo.x + dimension.leftLogo.width,
+          offset.outerBox.y + offset.leftLogo.y + dimension.leftLogo.height,
+        ],
+        [
+          offset.outerBox.x + offset.leftLogo.x,
+          offset.outerBox.y + offset.leftLogo.y + dimension.leftLogo.height,
+        ],
+      ],
+      {
+        stroke: "gray",
+        fill: "rgba(255,255,255,0.6)",
+        fillStyle: "solid",
+        roughness: 1,
+        strokeWidth: 2,
       }
     )
   );
